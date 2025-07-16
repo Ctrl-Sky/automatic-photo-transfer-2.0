b156ca8915cd60289298962c04b497cde5e9c66c
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-15 21:48:08 -0400

    create tests for copy file

--- transfer_sd_to_hd/tests/test_transfer_photos.py
+++ transfer_sd_to_hd/tests/test_transfer_photos.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from datetime import datetime
 from src.transfer_photos import is_lowest_date, image_before_end_on_date, copy_file_to_path
 from src.helpers import get_date_taken, convert_to_pretty_date
@@ -20,10 +21,27 @@ def test_image_before_end_on_date_is_before():
 def test_image_before_end_on_date_is_not_before():
     assert image_before_end_on_date("2025:04:06", datetime(2025, 5, 6, 2, 5, 3)) == False
 
-def test_copy_file_to_path():
+def test_copy_file_to_path_dir_exists():
     path_to_copy_dir = f"{PATH_TO_PHOTOS}/2025-transfer/Jul/Jul_06-exif"
     path_to_copy=f"{path_to_copy_dir}/IMG_0034.HEIC"
+
     copy_file_to_path(f"{PATH_TO_PHOTOS}/IMG_0034.HEIC", path_to_copy_dir)
     assert os.path.exists(path_to_copy)
     assert get_date_taken(path_to_copy)[1].strftime("%Y-%b-%d") == "2024-Jun-30"
+
+    # Clean up
     os.remove(path_to_copy)
+
+def test_copy_file_path_dir_not_exists():
+    dir = "2024-transfer"
+    image = "IMG_0034.HEIC"
+    path_to_copy_dir = f"{PATH_TO_PHOTOS}/{dir}/Jul/Jul_06-exif"
+    path_to_copy=f"{path_to_copy_dir}/{image}"
+
+    copy_file_to_path(f"{PATH_TO_PHOTOS}/{image}", path_to_copy_dir)
+    assert os.path.exists(path_to_copy)
+    assert get_date_taken(path_to_copy)[1].strftime("%Y-%b-%d") == "2024-Jun-30"
+
+    # Clean up
+    os.remove(path_to_copy)
+    shutil.rmtree(f"{PATH_TO_PHOTOS}/2024-transfer")
