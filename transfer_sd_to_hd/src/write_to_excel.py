260cbbb10f802ee8ca46ef127922f0c0e9b0b5ec
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-18 21:02:47 -0400

    finish creating write to migration table

--- transfer_sd_to_hd/src/write_to_excel.py
+++ transfer_sd_to_hd/src/write_to_excel.py
@@ -2,15 +2,13 @@ import csv
 import os
 from helpers import convert_to_month_year
 
-def write_to_migration_table(sd_card_path, start_dir, start_image, start_date, end_dir, end_image, end_date, table_path, migration_name=""):
+def write_to_migration_table(device, start_dir, start_image, start_date, end_dir, end_image, end_date, table_path, migration_name=""):
     if migration_name == "":
-        pretty_start = convert_to_month_year(start_date, include_day=True)
-        pretty_end = convert_to_month_year(end_date, include_day=True)
+        pretty_start = start_date.split()[0]
+        pretty_end = end_date.split()[0]
         migration_name = f"{pretty_start}_{pretty_end}"
 
-    sd_card_name = os.path.basename(sd_card_path)
-
-    data = [migration_name, sd_card_name, start_dir, start_image, start_date, end_dir, end_image, end_date]
+    data = [migration_name, device, start_dir, start_image, start_date, end_dir, end_image, end_date]
 
     if not os.path.exists(table_path):
         raise Exception(f"{table_path} does not exist")
