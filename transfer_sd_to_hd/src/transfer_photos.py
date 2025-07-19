43fef84d576a2373bad554189bd9654a7a1c9328
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-18 22:05:57 -0400

    create set new creation date

--- transfer_sd_to_hd/src/transfer_photos.py
+++ transfer_sd_to_hd/src/transfer_photos.py
@@ -37,6 +37,7 @@ def copy_file_to_path(file, path):
 
 def set_new_creation_date(path_to_file, new_date):
     cmd = 'SetFile -d ' + f'"{new_date.strftime("%m/%d/%Y %H:%M:%S")}" ' + f'"{path_to_file}"'
+    print(cmd)
     call(cmd, shell=True)
 
 def transfer_photos(start_date, external_hd_path, path_to_photos, end_on=""):
@@ -90,7 +91,7 @@ def transfer_photos(start_date, external_hd_path, path_to_photos, end_on=""):
                 new_path_to_photos = f"{external_hd_path}/{year}-transfer/{month}/{month}_{day}"
 
                 copy_file_to_path(path_to_photo, new_path_to_photos)
-                set_new_creation_date(path_to_photo, date)
+                set_new_creation_date(f"{new_path_to_photos}/{photo_name}", date)
 
                 if is_lowest_date(lowest_date, date):
                     # Starter values are written into the csv file for tracking
